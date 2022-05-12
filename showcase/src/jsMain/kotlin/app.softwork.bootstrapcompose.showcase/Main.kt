45623be3aa3d51175c04a8ba78fd64541f207cea
Luca Spinazzola <mspinluca@gmail.com>
2022-05-12 00:36:19 -0400

    NavbarDropDown issue

--- showcase/src/jsMain/kotlin/app.softwork.bootstrapcompose.showcase/Main.kt
+++ showcase/src/jsMain/kotlin/app.softwork.bootstrapcompose.showcase/Main.kt
@@ -5,12 +5,9 @@ import app.softwork.bootstrapcompose.*
 import org.jetbrains.compose.web.*
 import org.jetbrains.compose.web.dom.*
 
-data class AppState(val activePage: ActivePage)
 
 fun main() {
     renderComposable(rootElementId = "root") {
-        var state by remember { mutableStateOf(AppState(ActivePage.Box)) }
-
         Navbar(
             placement = NavbarPlacement.StickyTop,
             collapseBehavior = NavbarCollapseBehavior.AtBreakpoint(Breakpoint.Large),
@@ -24,13 +21,6 @@ fun main() {
             },
             navAttrs = { classes("flex-grow-1") }
         ) {
-            NavbarDropDown(title = state.activePage.displayName, href = "#") {
-                ActivePage.values().forEach {
-                    Button(it.displayName) {
-                        state = state.copy(activePage = it)
-                    }
-                }
-            }
             NavbarDropDown(title = "Login", href = "#") {
                 Custom {
                     RawInputView()
@@ -45,24 +35,6 @@ fun main() {
         }
 
         Main {
-            when (state.activePage) {
-                ActivePage.Alert -> AlertView()
-                ActivePage.Autocomplete -> AutoCompleteView()
-                ActivePage.Box -> BoxView()
-                ActivePage.ChecksAndRadios -> ChecksAndRadiosView()
-                ActivePage.DropDown -> DropDownView()
-                ActivePage.Form -> FormView()
-                ActivePage.Grid -> GridBoxView()
-                ActivePage.InputGroup -> InputGroupView()
-                ActivePage.ListGroup -> ListGroupView()
-                ActivePage.Modal -> ModalView()
-                ActivePage.Navbar -> NavbarView()
-                ActivePage.Offcanvas -> OffcanvasView()
-                ActivePage.Range -> RangeView()
-                ActivePage.Select -> SelectView()
-                ActivePage.Table -> TableView()
-                ActivePage.Toast -> ToastView()
-            }
         }
         Footer(attrs = { classes("footer", "mt-auto") }) {
             Container {
@@ -73,24 +45,5 @@ fun main() {
             }
         }
     }
-}
-
-enum class ActivePage(val displayName: String) {
-    Alert("Alert"),
-    Autocomplete("Autocomplete"),
-    Box("Boxes"),
-    ChecksAndRadios("Checks & radios"),
-    DropDown("Dropdowns"),
-    Form("Forms"),
-    Grid("Grid Layout"),
-    InputGroup("Input group"),
-    ListGroup("Lists"),
-    Modal("Modal"),
-    Navbar("Navbars"),
-    Offcanvas("Offcanvas"),
-    Range("Range"),
-    Select("Select"),
-    Table("Table"),
-    Toast("Toasts")
 }
 
--- showcase/src/jsMain/kotlin/app.softwork.bootstrapcompose.showcase/ModalView.kt
+++ showcase/src/jsMain/kotlin/app.softwork.bootstrapcompose.showcase/ModalView.kt
@@ -1,41 +1,41 @@
-package app.softwork.bootstrapcompose.showcase
-
-import androidx.compose.runtime.*
-import app.softwork.bootstrapcompose.*
-import org.jetbrains.compose.web.dom.*
-
-@Composable
-fun ModalView() {
-    var showModal by remember { mutableStateOf(false) }
-    var items: List<String> by remember { mutableStateOf(listOf()) }
-
-    Box(styling = { Padding { All { size = SpacingSpecs.SpacingSize.Small } } }) {
-        Button("Open Modal") {
-            showModal = true
-        }
-
-        if (showModal) {
-            Modal(
-                "Header",
-                onDismissRequest = { showModal = false },
-                footer = {
-                    Button("Close") {
-                        showModal = false
-                    }
-                }
-            ) {
-                Button("add item") {
-                    items = items + "Item ${items.size + 1}"
-                }
-
-                ListGroup {
-                    items.forEach {
-                        ListItem {
-                            Text(it)
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
+//package app.softwork.bootstrapcompose.showcase
+//
+//import androidx.compose.runtime.*
+//import app.softwork.bootstrapcompose.*
+//import org.jetbrains.compose.web.dom.*
+//
+//@Composable
+//fun ModalView() {
+//    var showModal by remember { mutableStateOf(false) }
+//    var items: List<String> by remember { mutableStateOf(listOf()) }
+//
+//    Box(styling = { Padding { All { size = SpacingSpecs.SpacingSize.Small } } }) {
+//        Button("Open Modal") {
+//            showModal = true
+//        }
+//
+//        if (showModal) {
+//            Modal(
+//                "Header",
+//                onDismissRequest = { showModal = false },
+//                footer = {
+//                    Button("Close") {
+//                        showModal = false
+//                    }
+//                }
+//            ) {
+//                Button("add item") {
+//                    items = items + "Item ${items.size + 1}"
+//                }
+//
+//                ListGroup {
+//                    items.forEach {
+//                        ListItem {
+//                            Text(it)
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//    }
+//}
